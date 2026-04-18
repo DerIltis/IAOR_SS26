31f431ad5c623b44a13fc90d3a45abf7525eae00
DerIltis <Der_Iltis@web.de>
2026-04-18 21:01:09 +0200

    feat: add show_np_array

--- src/assignment01.py
+++ src/assignment01.py
@@ -1,9 +1,26 @@
 import numpy as np
+import utils
+import matplotlib.pyplot as plt
 from PIL import Image
 
-def open_rgb_image(filepath):
-    image = Image.open(filepath)
-    image.load()
-    data = np.asarray(image, dtype=np.uint8)
-    return data
+def open_rgb_image_as_np_array(filepath):
+    rgb_image = Image.open(filepath)
+    rgb_image.load()
+    rgb_data = np.asarray(rgb_image, dtype=np.uint8)
+    return rgb_data
 
+def show_np_array(img):
+    fig = plt.figure()
+    plt.imshow(img)
+    return fig
+
+rgb_image = open_rgb_image_as_np_array("../images/input_sat_image.jpg")
+show_np_array(rgb_image)
+
+gs_image = utils.rgb2gray(rgb_image)
+show_np_array(gs_image)
+
+
+
+
+plt.show()
